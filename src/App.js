9762649bb89cf77f84edfc47787635290610a872
Saireddy0139 <saireddy0072002@gmail.com>
2024-02-29 11:48:17 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,59 +1,295 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import { BrowserRouter as Router,
-   Routes, 
-   Route,
-   Navigate
-   } from 'react-router-dom'; 
-//    import "@aws-amplify/ui-react/styles.css";
-// import {
-// withAuthenticator,
-// Button,
-// Heading,
-// Image,
-// View,
-// Card,
-// } from "@aws-amplify/ui-react"
-import SignUp from './Components/SignUp';
-import Login from './Components/Login';
- import ResetPassword from './Components/ResetPassword';
+// import React from 'react';
+// import { BrowserRouter as Router,
+//    Routes, 
+//    Route,
+//    Navigate
+//    } from 'react-router-dom'; 
+   import "@aws-amplify/ui-react/styles.css";
+import {
+Authenticator,useTheme,Text,
+Button,
+Heading,
+Image,
+View,
+Card,
+} from "@aws-amplify/ui-react"
+// import SignUp from './Components/SignUp';
+// import Login from './Components/Login';
+//  import ResetPassword from './Components/ResetPassword';
 
 
  
 // function App({ signOut }) {
-  function App() {
-  return (
-      <>
-      {/* <View className="App">
-        <Card> */}
+// //   function App() {
+//   return (
+//       <>
+//       <View className="App">
+//         <Card>
          
           
-          <Router>
-              <Routes>
-                  <Route
-                      exact
-                      path="/"
-                      element={<SignUp />} />
-                  <Route
-                      path="/login"
-                      element={<Login />} />
-                  { <Route
-                      path="/resetPassword"
-                      element={<ResetPassword />} /> }
+//           <Router>
+//               <Routes>
+//                   <Route
+//                       exact
+//                       path="/"
+//                       element={<SignUp />} />
+//                   <Route
+//                       path="/login"
+//                       element={<Login />} />
+//                   { <Route
+//                       path="/resetPassword"
+//                       element={<ResetPassword />} /> }
 
-                  <Route
-                      path="*"
-                      element={<Navigate to="/" />}
-                  />
-              </Routes>
-          </Router>
-          {/* <Heading level={1}></Heading>
-          </Card>
-          {/* <Button onClick={signOut}>Logout</Button> */}
-          {/* </View>  */}
-          </>
-  );
-}
-export default App;
-// export default withAuthenticator(App);
+//                   <Route
+//                       path="*"
+//                       element={<Navigate to="/" />}
+//                   />
+//               </Routes>
+//           </Router>
+//           <Heading level={1}></Heading>
+//           </Card>
+//           {<Button onClick={signOut}>Signout</Button> }
+//           </View> 
+//           </>
+//   );
+// }
+// //export default App;
+//  export default withAuthenticator(App);
+const components = {
+    Header() {
+      const { tokens } = useTheme();
+  
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Image
+            alt="Amplify logo"
+            //src="https://docs.amplify.aws/assets/logo-dark.svg"
+            src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=300,fit=crop,q=100/mxBjreBL9ZfB5Gob/microsoftteams-image-d95Dp2xwZMSOW8bQ.png"
+            style={{width: '250px,100px', height: '1', padding: '0px' }}
+          />
+        </View>
+      );
+    },
+  
+    Footer() {
+      const { tokens } = useTheme();
+  
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Text color={tokens.colors.neutral[80]}>
+            &copy; All Rights Reserved
+          </Text>
+        </View>
+      );
+    },
+  
+    SignIn: {
+      Header() {
+        const { tokens } = useTheme();
+  
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Sign in to your account
+          </Heading>
+        );
+      },
+      Footer() {
+        const { toForgotPassword } = Authenticator();
+  
+        return (
+          <View textAlign="center">
+            <Button
+              fontWeight="normal"
+              onClick={toForgotPassword}
+              size="small"
+              variation="link"
+            >
+              Reset Password
+            </Button>
+          </View>
+        );
+      },
+    },
+  
+    SignUp: {
+      Header() {
+        const { tokens } = useTheme();
+  
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Create a new account
+          </Heading>
+        );
+      },
+      Footer() {
+        const { toSignIn } = Authenticator();
+  
+        return (
+          <View textAlign="center">
+            <Button
+              fontWeight="normal"
+              onClick={toSignIn}
+              size="small"
+              variation="link"
+            >
+              Back to Sign In
+            </Button>
+          </View>
+        );
+      },
+    },
+    ConfirmSignUp: {
+      Header() {
+        const { tokens } = useTheme();
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Enter Information:
+          </Heading>
+        );
+      },
+      Footer() {
+        return <Text>Footer Information</Text>;
+      },
+    },
+    SetupTotp: {
+      Header() {
+        const { tokens } = useTheme();
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Enter Information:
+          </Heading>
+        );
+      },
+      Footer() {
+        return <Text>Footer Information</Text>;
+      },
+    },
+    ConfirmSignIn: {
+      Header() {
+        const { tokens } = useTheme();
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Enter Information:
+          </Heading>
+        );
+      },
+      Footer() {
+        return <Text>Footer Information</Text>;
+      },
+    },
+    ForgotPassword: {
+      Header() {
+        const { tokens } = useTheme();
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Enter Information:
+          </Heading>
+        );
+      },
+      Footer() {
+        return <Text>Footer Information</Text>;
+      },
+    },
+    ConfirmResetPassword: {
+      Header() {
+        const { tokens } = useTheme();
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Enter Information:
+          </Heading>
+        );
+      },
+      Footer() {
+        return <Text>Footer Information</Text>;
+      },
+    },
+  };
+  
+  const formFields = {
+    signIn: {
+      username: {
+        placeholder: 'Enter your email',
+      },
+    },
+    signUp: {
+      password: {
+        label: 'Password:',
+        placeholder: 'Enter your Password:',
+        isRequired: false,
+        order: 2,
+      },
+      confirm_password: {
+        label: 'Confirm Password:',
+        order: 1,
+      },
+    },
+    forceNewPassword: {
+      password: {
+        placeholder: 'Enter your Password:',
+      },
+    },
+    forgotPassword: {
+      username: {
+        placeholder: 'Enter your email:',
+      },
+    },
+    confirmResetPassword: {
+      confirmation_code: {
+        placeholder: 'Enter your Confirmation Code:',
+        label: 'New Label',
+        isRequired: false,
+      },
+      confirm_password: {
+        placeholder: 'Enter your Password Please:',
+      },
+    },
+    setupTotp: {
+      QR: {
+        totpIssuer: 'test issuer',
+        totpUsername: 'amplify_qr_test_user',
+      },
+      confirmation_code: {
+        label: 'New Label',
+        placeholder: 'Enter your Confirmation Code:',
+        isRequired: false,
+      },
+    },
+    confirmSignIn: {
+      confirmation_code: {
+        label: 'New Label',
+        placeholder: 'Enter your Confirmation Code:',
+        isRequired: false,
+      },
+    },
+  };
+  
+  export default function App() {
+    return (
+      <Authenticator formFields={formFields} components={components}>
+        {({ signOut }) => <button onClick={signOut}>Sign out</button>}
+      </Authenticator>
+    );
+  }
